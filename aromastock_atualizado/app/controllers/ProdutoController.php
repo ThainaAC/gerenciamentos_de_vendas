70de814f56876833b3a6decde0c49d02a9a53628
gessicavz <gessicamatias28@gmail.com>
2026-06-19 17:35:44 -0300

    Update ProdutoController.php

--- aromastock_atualizado/app/controllers/ProdutoController.php
+++ aromastock_atualizado/app/controllers/ProdutoController.php
@@ -6,35 +6,97 @@ require_once "../app/models/Categoria.php";
 class ProdutoController {
     public function index() {
         Auth::verificar();
+
         $produtos = Produto::listar();
         $categorias = Categoria::listar();
+
         require "../app/views/produtos/index.php";
     }
 
     public function salvar() {
         Auth::verificar();
 
+        $categoria_id = $_POST['categoria_id'] ?? "";
+        $nome = $_POST['nome'] ?? "";
+        $descricao = $_POST['descricao'] ?? "";
+        $quantidade = $_POST['quantidade'] ?? "";
+        $preco_compra = $_POST['preco_compra'] ?? "";
+
         if (
-            Validator::inteiroPositivo($_POST['categoria_id'] ?? "") &&
-            Validator::texto($_POST['nome'] ?? "") &&
-            Validator::numero($_POST['quantidade'] ?? "") &&
-            Validator::numero($_POST['preco_compra'] ?? "")
+            Validator::inteiroPositivo($categoria_id) &&
+            Validator::texto($nome) &&
+            Validator::numero($quantidade) &&
+            Validator::numero($preco_compra)
         ) {
             Produto::salvar(
-                $_POST['categoria_id'],
-                $_POST['nome'],
-                $_POST['descricao'] ?? "",
-                $_POST['quantidade'],
-                $_POST['preco_compra']
+                $categoria_id,
+                $nome,
+                $descricao,
+                $quantidade,
+                $preco_compra
             );
         }
 
         header("Location: index.php?controller=produto&action=index");
+        exit;
+    }
+
+    public function editar() {
+        Auth::verificar();
+
+        $id = $_GET['id'] ?? 0;
+
+        $produto = Produto::listarPorId($id);
+        $categorias = Categoria::listar();
+
+        require "../app/views/produtos/editar_produtos.php";
+    }
+
+    public function atualizar() {
+        Auth::verificar();
+
+        $id = $_POST['id'] ?? "";
+        $categoria_id = $_POST['categoria_id'] ?? "";
+        $nome = $_POST['nome'] ?? "";
+        $descricao = $_POST['descricao'] ?? "";
+        $quantidade = $_POST['quantidade'] ?? "";
+        $preco_compra = $_POST['preco_compra'] ?? "";
+
+        if (
+            Validator::inteiroPositivo($id) &&
+            Validator::inteiroPositivo($categoria_id) &&
+            Validator::texto($nome) &&
+            Validator::numero($quantidade) &&
+            Validator::numero($preco_compra)
+        ) {
+            Produto::atualizar(
+                $id,
+                $categoria_id,
+                $nome,
+                $descricao,
+                $quantidade,
+                $preco_compra
+            );
+        }
+
+        header("Location: index.php?controller=produto&action=index&sucesso=produto_atualizado");
+        exit;
     }
 
     public function excluir() {
-        Auth::verificar();
-        Produto::excluir($_GET['id']);
-        header("Location: index.php?controller=produto&action=index");
+    Auth::verificar();
+
+    $id = $_GET['id'] ?? 0;
+
+    $resultado = Produto::excluir($id);
+
+    if ($resultado === false) {
+        header("Location: index.php?controller=produto&action=index&erro=produto_vinculado");
+        exit;
     }
+
+    header("Location: index.php?controller=produto&action=index&sucesso=produto_excluido");
+    exit;
 }
+}
+?>
