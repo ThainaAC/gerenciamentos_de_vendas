eac3f311b35350c06a2725e3a9cdfbf19b0d5108
gessicavz <gessicamatias28@gmail.com>
2026-06-19 17:35:54 -0300

    Update Categoria.php

--- aromastock_atualizado/app/models/Categoria.php
+++ aromastock_atualizado/app/models/Categoria.php
@@ -6,10 +6,26 @@ class Categoria {
         return $db->query("SELECT * FROM categorias ORDER BY nome")->fetchAll(PDO::FETCH_ASSOC);
     }
 
+      public static function listarPorId($id) {
+        $db = Database::conectar();
+
+        $stmt = $db->prepare("SELECT * FROM categorias WHERE id = ?");
+        $stmt->execute([$id]);
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
     public static function salvar($nome) {
         $db = Database::conectar();
         $stmt = $db->prepare("INSERT INTO categorias (nome) VALUES (?)");
         return $stmt->execute([$nome]);
+    }
+
+     public static function atualizar($id, $nome) {
+        $db = Database::conectar();
+
+        $stmt = $db->prepare("UPDATE categorias SET nome = ? WHERE id = ?");
+        return $stmt->execute([$nome, $id]);
     }
 
     public static function excluir($id) {
@@ -18,3 +34,4 @@ class Categoria {
         return $stmt->execute([$id]);
     }
 }
+
