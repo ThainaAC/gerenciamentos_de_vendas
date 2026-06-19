3064423cd0cd25a05977869b04959485964b9239
gessicavz <gessicamatias28@gmail.com>
2026-06-19 17:35:29 -0300

    Update CategoriaController.php

--- aromastock_atualizado/app/controllers/CategoriaController.php
+++ aromastock_atualizado/app/controllers/CategoriaController.php
@@ -19,6 +19,33 @@ class CategoriaController {
         header("Location: index.php?controller=categoria&action=index");
     }
 
+      public function editar() {
+        Auth::verificar();
+
+        $id = $_GET['id'] ?? 0;
+
+        $categoria = Categoria::listarPorId($id);
+
+        require "../app/views/categorias/editar_categorias.php";
+    }
+
+       public function atualizar() {
+        Auth::verificar();
+
+        $id = $_POST['id'] ?? "";
+        $nome = $_POST['nome'] ?? "";
+
+        if (
+            Validator::inteiroPositivo($id) &&
+            Validator::texto($nome)
+        ) {
+            Categoria::atualizar($id, $nome);
+        }
+
+        header("Location: index.php?controller=categoria&action=index&sucesso=categoria_atualizada");
+        exit;
+    }
+
     public function excluir() {
         Auth::verificar();
         Categoria::excluir($_GET['id']);
